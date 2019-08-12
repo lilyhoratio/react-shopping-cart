be09184399d9b9cacc83bd983ed5851dfc21e60b
lilyhoratio <lilyhzhou@gmail.com>
2019-08-12 14:34:39 -0700

    MVP complete. remove cart item function working

--- src/App.js
+++ src/App.js
@@ -14,14 +14,28 @@ import ShoppingCart from "./components/ShoppingCart";
 function App() {
   const [products] = useState(data);
   const [cart, setCart] = useState([]);
+  //   console.log("CART		:", cart);
 
   const addItem = item => {
     setCart([...cart, item]);
   };
 
+  const removeItem = itemId => {
+    // for all items currently in cart, isolate the ones that weren't removed/clicked. set state of cart to those remaining items
+
+    console.log(cart);
+    const filteredItems = cart.filter(cartItem => {
+      //   console.log(typeof cartItem.id);
+      //   console.log(typeof item.id);
+      return cartItem.id !== itemId;
+    });
+    // console.log("delete id", filteredItems);
+    setCart(filteredItems);
+  };
+
   return (
     <ProductContext.Provider value={{ products, addItem }}>
-      <CartContext.Provider value={{ cart, setCart }}>
+      <CartContext.Provider value={{ cart, setCart, removeItem }}>
         <div className="App">
           <Navigation cart={cart} />
 
